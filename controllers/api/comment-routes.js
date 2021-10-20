4c5b4448f0f932d31ffe47807b0f9d567433ebe3
JD Rowley <jdrowley1989@gmail.com>
2021-10-20 14:45:51 -0600

    add and tested comments routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,27 +3,64 @@ const { Comment } = require('../../models');
 
 // get all comments
 router.get('/', (req, res) => {
-
-});
-
-// get one comment
-router.get('/:id', (req, res) => {
-
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.json(500).json(err);
+        });
 });
 
 // post comment
 router.post('/', (req, res) => {
-
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // update comment
-router.put('/:id', (req, res) => {
-
-});
+// router.put('/:id', (req, res) => {
+//     Comment.update(req.body, {
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(dbCommentData => {
+//         if (!dbCommentData) {
+//             res.status(404).json({ message: 'No user found with this id' });
+//             return;
+//         }
+//         res.json(dbCommentData);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
 // delete comment
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
