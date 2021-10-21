50faee986e5379c641b52c85332ede6fbabcee3c
JD Rowley <jdrowley1989@gmail.com>
2021-10-21 11:14:57 -0600

    updates to post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -11,6 +11,13 @@ router.get('/', (req, res) => {
             'title',
             'created_at'
         ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -24,7 +31,19 @@ router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
-        }
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     }).then(dbPostData => {
         if (!dbPostData) {
             res.status(404).json({ message: 'No post found with this id' });
