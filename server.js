4209be61cdbdf5fc6338deaa8c5df2ca6fd7d525
JD Rowley <jdrowley1989@gmail.com>
2021-10-22 10:33:56 -0600

    add login/logout buttons with functionality

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs =require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'It is a secret to everyone',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -13,6 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // use routes in controllers directory
 app.use(require('./controllers'));
